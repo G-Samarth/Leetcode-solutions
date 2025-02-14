f996e6a9145aca750cdba2752a43d10faac0b2d6
G-Samarth <54974925+G-Samarth@users.noreply.github.com>
2025-02-13 18:11:32 -0800

    Time: 46 ms (75.19%) | Memory: 29 MB (85.07%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -13,11 +13,11 @@ public:
                 if(nums[i] + nums[j] + nums[k] == 0){
                     ans.push_back({nums[i], nums[j], nums[k]});
                     j++;
-                    k--;
+                    // k--;
                     while(j<k && nums[j-1] == nums[j])
                         j++;
-                    while(j<k && nums[k+1] == nums[k])
-                        k--;
+                    // while(j<k && nums[k+1] == nums[k])
+                    //     k--;
                 }
                 else if(nums[i] + nums[j] + nums[k] < 0)
                     j++;
