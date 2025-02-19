cfc37f4506c72669193ad6098481f8c6642ca1cf
G-Samarth <54974925+G-Samarth@users.noreply.github.com>
2025-02-19 12:45:05 -0800

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.18%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,18 +4,20 @@ public:
         if(!nums.size())
             return 0;
         // DP
-        vector<int> dp(nums.size(), 0);
-        dp[0] = nums[0];
+        // vector<int> dp(nums.size(), 0);
+        int first = nums[0], second = first;
         if(nums.size() > 1)
-            dp[1] = max(nums[0],nums[1]);
+            second = max(nums[0],nums[1]);
         // [-,-,-,-]
         
         // For Loop
         for(int i=2;i<nums.size();i++){
             // To Rob or not to rob
-            dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+            int temp = max(nums[i] + first, second);
+            first = second;
+            second = temp;
         }
 
-        return dp[nums.size()-1];
+        return second;
     }
 };
