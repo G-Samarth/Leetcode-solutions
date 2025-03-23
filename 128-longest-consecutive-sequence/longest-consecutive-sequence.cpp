8985fbd470a86c14cd4b41f6aa9409161ef9162a
G-Samarth <54974925+G-Samarth@users.noreply.github.com>
2025-03-23 14:44:25 -0700

    Time: 295 ms (5.02%) | Memory: 91.3 MB (9.62%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,20 +3,30 @@ public:
     int longestConsecutive(vector<int>& nums) {
         map<int, int> map;
 
-        for(int i=0;i<nums.size();i++){
-            map[nums[i]] = 1;
+        for(int n : nums){
+            map[n] = -1;
         }
 
         int ans = 0;
-        for(auto m : map){
-            int temp = m.first;
-            int count = 0;
-            while(map[temp] == 1){
-                map[temp++] = 0;
-                count++;
-            }
+        for(auto n : nums){
+            if(map[n] == -1){
+                int curr = n;
+                int longest = 0;
 
-            ans = max(ans, count);
+                while(map.find(curr) != map.end()){
+                    if(map[curr] != -1){
+                        longest += map[curr];
+                        break;
+                    }else{
+                        map[curr] = 0;
+                        longest++;
+                    }
+                    curr++;
+                }
+
+                ans = max(longest, ans);
+                map[n] = longest;
+            }
         }
 
         return ans;
