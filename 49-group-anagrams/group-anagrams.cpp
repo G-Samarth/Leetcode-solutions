0164cba5cf83a1915ce9932ba08066f71d522fcf
G-Samarth <54974925+G-Samarth@users.noreply.github.com>
2025-12-03 13:12:25 -0800

    Time: 11 ms (90.90%) | Memory: 26 MB (53.35%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,14 +3,16 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> map;
 
-        for(string s : strs){
-            string temp = s;
-            sort(s.begin(), s.end());
-            map[s].push_back(temp);
+        for(string str : strs){
+            string copy = str;
+            sort(copy.begin(), copy.end());
+
+            map[copy].push_back(str);
         }
 
         vector<vector<string>> ans;
-        for(auto m : map){ 
+
+        for(auto m : map){
             ans.push_back(m.second);
         }
 
