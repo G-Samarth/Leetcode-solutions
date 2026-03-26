f0b25d0bae6f4edbed43d18e6695c3449e870275
G-Samarth <54974925+G-Samarth@users.noreply.github.com>
2026-03-25 22:07:55 -0700

    0017-LETTER-COMBINATIONS-OF-A-PHONE-NUMBER Stats: Time: 0 ms (100%), Space: 9.1 MB (75.82%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,19 +1,26 @@
 class Solution {
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl",
+                          "mno", "pqrs", "tuv", "wxyz"};
 public:
-    vector<string> letterCombinations(string digits) {
-        vector<string> ans = {""};
-        vector<string> mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-        for(char d : digits){
-            vector<string> temp;
-            for(auto s : ans){
-                int num = d - '0';
-                for(auto c : mapping[num])
-                    temp.push_back(s + c);
-            }
-            ans = temp;
+    void getAllCombos(string digits, string curr, vector<string>& ans, int index){
+        if(index == digits.size()){
+            ans.push_back(curr);
+            return;
         }
 
+        for(auto m : mapping[digits[index] - '0']){
+            curr.push_back(m);
+            getAllCombos(digits, curr, ans, index+1);
+            curr.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(!digits.size())
+            return {};
+        
+        string curr = "";
+        vector<string> ans;
+        getAllCombos(digits, curr, ans, 0);
         return ans;
     }
 };
