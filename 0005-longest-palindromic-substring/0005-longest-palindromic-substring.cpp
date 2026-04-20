9acde08074a17e0c7c171c7b1e93cebcea69a1d8
G-Samarth <54974925+G-Samarth@users.noreply.github.com>
2026-04-19 21:07:16 -0700

    0005-LONGEST-PALINDROMIC-SUBSTRING Stats: Time: 193 ms (19.07%), Space: 292 MB (6.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -6,26 +6,24 @@ public:
 
         int start = 0, len = 1;
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<s.length();i++)
             dp[i][i] = 1;
-
-        for(int i=0;i<n-1;i++){
+        
+        for(int i=0;i<s.length()-1;i++){
             if(s[i] == s[i+1]){
                 dp[i][i+1] = 1;
-
-                len = 2;
                 start = i;
+                len = 2;
             }
         }
 
-        for(int g=2;g<n;g++){
-            for(int i=0, j=g;j<n;i++,j++){
+        for(int g=2;g<s.length();g++){
+            for(int i=0, j=g;j<s.length();i++,j++){
                 if(s[i] == s[j]){
                     if(dp[i+1][j-1]){
-                        dp[i][j] = 1;
-
-                        len = g+1;
                         start = i;
+                        len = g+1;
+                        dp[i][j] = 1;
                     }
                 }
             }
