dadfe9a21461a6457e7f752532289a250db97134
G-Samarth <54974925+G-Samarth@users.noreply.github.com>
2026-04-19 21:51:59 -0700

    0042-TRAPPING-RAIN-WATER Stats: Time: 0 ms (100%), Space: 27.3 MB (25.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,18 +4,32 @@ public:
         int n = height.size();
         vector<int> leftMax(n, 0), rightMax(n, 0);
 
+        // int left = 0;
+        // for(int i=0;i<n;i++){
+        //     leftMax[i] = left;
+        //     left = max(left, height[i]);
+        // }
+
+        // int right = 0;
+        // for(int i=n-1;i>=0;i--){
+        //     rightMax[i] = right;
+        //     right = max(right, height[i]);
+        // }
+
         leftMax[0] = height[0];
-        for(int i=1;i<n;i++)
+        for(int i=1;i<n;i++){
             leftMax[i] = max(leftMax[i-1], height[i]);
-        
+        }
+
         rightMax[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--)
+        for(int i=n-2;i>=0;i--){
             rightMax[i] = max(rightMax[i+1], height[i]);
+        }
 
         int ans = 0;
-
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++){
             ans += min(leftMax[i], rightMax[i]) - height[i];
+        }
 
         return ans;
     }
